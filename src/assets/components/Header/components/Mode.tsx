04e25d6d48ac1ebd619a7f6742db36ed9819a91b
kimjihu0 <sh9732626@gmail.com>
2024-04-23 23:30:11 +0900

    fix : theme state App컴포넌트로 추출 및 LocalStorage 작업 완료

--- src/assets/components/Header/components/Mode.tsx
+++ src/assets/components/Header/components/Mode.tsx
@@ -1,17 +1,12 @@
-import { useState, useEffect } from 'react';
-
-const Mode = () => {
-    const [theme, setTheme] = useState('light');
-
-    useEffect(() => {
-        if (theme === 'dark') document.querySelector('html')!.classList.add('dark');
-        else document.querySelector('html')!.classList.remove('dark');
-    }, [theme]);
-
+interface ModelProps {
+    theme: string;
+    onClickMode: () => void;
+}
+const Mode = ({ theme, onClickMode }: ModelProps) => {
     return (
         <div className="grid place-items-center">
             <button
-                onClick={() => setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'))}
+                onClick={onClickMode}
                 className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50  h-10 px-4 py-2 rounded-lg"
                 data-id="13"
             >
