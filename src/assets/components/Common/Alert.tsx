d1f2894ab954bba1ae909e6d31ed38637c5d3f77
kimjihu0 <sh9732626@gmail.com>
2024-04-23 23:30:39 +0900

    fix : alert CSS 수정

--- src/assets/components/Common/Alert.tsx
+++ src/assets/components/Common/Alert.tsx
@@ -5,9 +5,9 @@ interface AlertProps {
 const Alert = ({ alertMessage, onAlertClose }: AlertProps) => {
     return (
         <div className="fixed z-50 flex items-center justify-center">
-            <div className="fixed left-5 top-10 flex flex-col bg-white dark:bg-slate-700 dark:border-black dark:text-gray-300 rounded-lg">
+            <div className="fixed left-5 top-10 flex flex-col bg-white dark:bg-slate-500 dark:border-black dark:text-gray-300 rounded-lg">
                 <div className="flex justify-between md:p-2">
-                    <span className="flex items-center text-lg font-semibold">{alertMessage}</span>
+                    <span className="flex items-center text-lg font-semibold px-3 text-red-300">{alertMessage}</span>
                     <button onClick={onAlertClose}>
                         <img src="/image/icon/CloseIcon.png" className="p-2 w-8 object-contain opacity-50" />
                     </button>
