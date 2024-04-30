e240382b43a74926280bbc2de29e6feb14cd01e5
kimjihu0 <sh9732626@gmail.com>
2024-04-30 19:14:18 +0900

    fix : sidemenu 추가

--- src/assets/components/SideBar/SideBar.tsx
+++ src/assets/components/SideBar/SideBar.tsx
@@ -1,23 +1,41 @@
+import { NavLink } from 'react-router-dom';
+
 interface SideBarType {
     isSide: boolean;
     onToggleIsSide: () => void;
 }
 
 const Sidebar = ({ isSide, onToggleIsSide }: SideBarType) => {
+    const menuList = [
+        {
+            title: '닉네임 검색',
+            path: '/',
+        },
+        {
+            title: 'Stove API 발급 방법',
+            path: '/guide',
+        },
+    ];
     return (
         <div
-            className={`fixed inset-y-0 left-0 z-10 w-64 bg-gray-800 transition duration-300 ease-in-out transform ${
+            className={`fixed inset-y-0 left-0 z-10 w-64 bg-gray-200 dark:bg-gray-800 transition duration-200 ease-in-out transform ${
                 isSide ? 'translate-x-0 shadow-r' : '-translate-x-full'
             }`}
         >
             <div className="flex justify-between p-4">
-                <button className="text-white" onClick={onToggleIsSide}>
+                <button className="dark:text-white" onClick={onToggleIsSide}>
                     X
                 </button>
             </div>
-            <div className="p-4">
+            <div className="flex flex-col p-4 gap-5">
                 {/* Sidebar content goes here */}
-                <p className="text-white">Sidebar Content</p>
+                {menuList.map((menu, index) => (
+                    <p key={index} className="dark:text-white text-lg">
+                        <NavLink className="active:text-neutral-200" to={menu.path}>
+                            {menu.title}
+                        </NavLink>
+                    </p>
+                ))}
             </div>
         </div>
     );
