3effc4d18b9b17910a470c78146b7fffea044be4
kimjihu0 <sh9732626@gmail.com>
2024-05-08 20:26:59 +0900

    style: 주석 제거

--- src/assets/ts/api/api.ts
+++ src/assets/ts/api/api.ts
@@ -12,36 +12,4 @@ const api = (apiKey: string, nickname: string) =>
         })
         .get(`/characters/${nickname}/siblings`);
 
-// const api = (apiKey) => {
-//     const axiosInstance = axios.create({
-//         baseURL: `https://developer-lostark.game.onstove.com/characters`,
-//         headers: {
-//             accept: 'application/json',
-//             authorization: `bearer ${apiKey}`,
-//         },
-//     });
-
-//     // 성공 시
-//     const onFulfilled = (response) => response;
-//     // 재시도
-//     const retry = (errorConfig) => {
-//         console.log('@@@@@@@@@@@@@@@@@@@ re-try @@@@@@@@@@@@@@@@@@@');
-//         return new Promise((resolve) => {
-//             resolve(axiosInstance.request(errorConfig));
-//         });
-//     };
-//     // 실패 시
-//     const onRejected = (error) => {
-//         console.log('onReject : ', error);
-//         if (error.config) {
-//             return retry(error.config);
-//         }
-//         return Promise.reject(error);
-//     };
-
-//     axiosInstance.interceptors.response.use(onFulfilled, onRejected);
-
-//     return axiosInstance;
-// };
-
 export default api;
